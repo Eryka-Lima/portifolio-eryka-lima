bb16d1b7fd25efe48b8ea71969c7d8cd84d2f0c8
Eryka Lima <erykalima.contato@gmail.com>
2025-11-17 22:09:46 -0300

    Portifolio continuação

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
     <div>
-      <h1>Eryka Lima</h1>
+      <h1>ERYKA LIMA DA SILVA</h1>
+
+      <Image 
+        src="/img1.jpg" 
+        alt="Foto exemplo" 
+        width={500}
+        height={300}
+        />
     </div>
+
   );
 }
